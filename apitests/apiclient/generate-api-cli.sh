5bd0051eb583a5cb635803a57632e715cbc94928
pmoskovkin <pavel.moskovkin@insolar.io>
2019-08-02 17:00:11 +0300

    cli codegen from template

--- apitests/apiclient/generate-api-cli.sh
+++ apitests/apiclient/generate-api-cli.sh
@@ -4,27 +4,38 @@ set -x
 
 WORKDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 SPEC_BASE_DIR=${WORKDIR}
-REPOS=( "insolar-observer-api" "insolar-internal-api" "insolar-api" )
+#REPOS=( "insolar-observer-api" "insolar-internal-api" "insolar-api" )
+#REPOS=( "insolar-api" )
+#
+#for repo_name in "${REPOS[@]}"
+#do
+#  if [[ ! -f ${SPEC_BASE_DIR}/${repo_name} ]]; then
+#	  git clone git@github.com:insolar/${repo_name}.git ${SPEC_BASE_DIR}/${repo_name}
+#	fi
+#	cd ${SPEC_BASE_DIR}/${repo_name} || exit 0
+#  if [[ ${repo_name} == "insolar-api" ]]; then
+#    git checkout 1.0.0
+#    git pull
+#  else
+#    git pull origin master
+#  fi
+#
+#	npm install
+#  npm run export -- --collapse
+#  mkdir openapi
+#  openapi-generator generate \
+#      --input-spec api-exported.yaml \
+#      --generator-name go \
+#      --output ${SPEC_BASE_DIR}/${repo_name} \
+#      --package-name insolar-api-cli \
+#      --skip-validate-spec
+#done
 
-for repo_name in "${REPOS[@]}"
-do
-  if [[ ! -f ${SPEC_BASE_DIR}/${repo_name} ]]; then
-	  git clone git@github.com:insolar/${repo_name}.git ${SPEC_BASE_DIR}/${repo_name}
-	fi
-	cd ${SPEC_BASE_DIR}/${repo_name} || exit 0
-  if [[ ${repo_name} == "insolar-api" ]]; then
-    git checkout 1.0.0
-    git pull
-  else
-    git pull origin master
-  fi
-
-	npm install
-  npm run export -- --collapse
-  mkdir openapi
-  openapi-generator generate \
-      --input-spec api-exported.yaml \
+# TODO kostil
+openapi-generator generate \
+      --input-spec api-exported-template.yaml \
       --generator-name go \
-      --output ${SPEC_BASE_DIR}/${repo_name}/openapi \
+      --output ${SPEC_BASE_DIR}/apiclient \
+      --package-name apiclient \
+      --model-package models \
       --skip-validate-spec
-done
